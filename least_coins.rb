89eeeb56fdc6a8930061cdb0dbbd3cf8522d0953
angeljolon <angeljolon@yahoo.com>
2018-07-12 18:22:50 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,17 @@
 #write out your code here
 
 def least_coins(cents)
-
+  
+  coin_values = {
+    "quarters" => 0, 
+    "dimes" => 0, 
+    "nickels" => 0,
+    "pennies" => 0
+  }
+  if cents >= 
 #Code your answer here!
+puts 
+
+
 
 end
